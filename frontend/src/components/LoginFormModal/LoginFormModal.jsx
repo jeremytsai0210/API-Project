bdcdb487ff457bb0910d7ffa3b104cb068d56eb9
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-10-14 15:32:15 -0700

    Setting up Render

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import * as sessionActions from '../../store/session';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
 import './LoginForm.css';
 
