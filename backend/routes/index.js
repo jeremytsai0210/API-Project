d4d2bd5147793c852f26149995a3a2ba780fd6f4
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-10-15 08:29:58 -0700

    Comment in csrf imports

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const apiRouter = require("./api");
-// const csrf = require("csurf");
-// const csrfProtection = csrf({ cookie: true });
+const csrf = require("csurf");
+const csrfProtection = csrf({ cookie: true });
 
-// router.use(csrfProtection);
+router.use(csrfProtection);
 
 router.use("/api", apiRouter);
 
