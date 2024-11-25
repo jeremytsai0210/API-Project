ca0113a4b7aa7eb9682e7828bfc19f89c76f5f7a
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-11-25 11:16:47 -0800

    Tried adding images but broke migration and seeding

--- backend/db/seeders/20240924213910-demo-spot-image.js
+++ backend/db/seeders/20240924213910-demo-spot-image.js
@@ -23,7 +23,7 @@ module.exports = {
       [
         {
           spotId: 1,
-          url: "/images/SanFrancisco/1.png",
+          url: "https://images.unsplash.com/photo-1567371891232-7265b51bab42?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           preview: true,
         },
         {
